0436ba959f2f770bad10e298925bb9371d47a8a6
Freddy Luis Paredes B <fparedes@mardom.com>
2021-07-30 09:21:15 -0400

    Updated DtoMetadata for nullable types

--- Api.Net.Core/Metatada/DtoMetadata.cs
+++ Api.Net.Core/Metatada/DtoMetadata.cs
@@ -9,25 +9,19 @@ namespace Api.Net.Core.Metatada
 {
     public class DtoMetadata
     {
-        private static DtoMetadata _instance;
-        public static DtoMetadata Instance
-        {
-            get
-            {
-                if (_instance is null)
-                    _instance = new DtoMetadata();
-                return _instance;
-            }
-        }
+        public static DtoMetadata Instance { get; } = new DtoMetadata();
+
         private DtoMetadata()
         {
             Projections = new Dictionary<Type, IEnumerable<ProjectionDefinition>>();
         }
 
         public Dictionary<Type, IEnumerable<ProjectionDefinition>> Projections { get; set; }
-        public ApiConvention Convention { get; set; }
 
-        public ProjectionDefinition ResolveProyection(Type dtoType, string name)
+        #warning This is not null safe, will be null if `AddApi` is not called
+        public ApiConvention Convention { get; set; } = default!;
+
+        public ProjectionDefinition? ResolveProyection(Type dtoType, string name)
         {
             if (!Projections.ContainsKey(dtoType)) return null;
             return Projections[dtoType].FirstOrDefault(t => t.Name == name);
