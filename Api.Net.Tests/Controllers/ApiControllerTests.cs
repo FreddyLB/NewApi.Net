20cc7e0c239eaa3ca11b08cfca595228bef66af9
Freddy Luis Paredes B <fparedes@mardom.com>
2021-08-04 16:55:14 -0400

    Added custom http context for ApiController

--- Api.Net.Tests/Controllers/ApiControllerTests.cs
+++ Api.Net.Tests/Controllers/ApiControllerTests.cs
@@ -1,11 +1,16 @@
 ﻿using Api.Attributes;
 using Api.Controllers;
 using Api.Dto.Base;
+using Api.Models;
 using Api.Parameters;
 using Api.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+using Moq;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -14,57 +19,6 @@ using Xunit;
 
 namespace Api.Net.Tests.Controllers
 {
-    [ApiEndpoint("persons")]
-    public class PersonDto : Dto<PersonDto, Person>
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-
-        public int Age { get; set; }
-
-        public override bool Equals(object obj)
-        {
-            return obj is PersonDto dto &&
-                   Id == dto.Id &&
-                   Name == dto.Name &&
-                   Age == dto.Age;
-        }
-
-        public override int GetHashCode()
-        {
-            return HashCode.Combine(Id, Name, Age);
-        }
-    }
-
-    public class Person
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-
-        public int Age { get; set; }
-
-        public override bool Equals(object obj)
-        {
-            return obj is Person dto &&
-                   Id == dto.Id &&
-                   Name == dto.Name &&
-                   Age == dto.Age;
-        }
-
-        public override int GetHashCode()
-        {
-            return HashCode.Combine(Id, Name, Age);
-        }
-    }
-
-    public class PersonDbContext : DbContext
-    {
-        public PersonDbContext(DbContextOptions<PersonDbContext> options) : base(options) { }
-
-        public DbSet<Person> People { get; set; }
-    }
-
-
     public class ApiControllerTests
     {
         private static IServiceProvider Provider = GetServiceProvider();
@@ -73,19 +27,30 @@ namespace Api.Net.Tests.Controllers
         public void FindTest()
         {
             var controller = GetController<PersonDto>();
-            //controller.Add(new PersonDto { Name = "Ana", Age = 23 });
-            //controller.Add(new PersonDto { Name = "Maria", Age = 30 });
 
-            //var result = controller.GetAll(new ApiParameter());
-            //Console.WriteLine(result);
-            //Assert.Empty(result.Value as IEnumerable<PersonDto>);            
+            controller.Add(new PersonDto { Name = "Ana", Age = 23 });
+            controller.Add(new PersonDto { Name = "Maria", Age = 30 });
+
+            var actionResult = controller.GetAll(new ApiParameter());
+            var okResult = actionResult.Result as OkObjectResult;
+            var value = okResult.Value as ListResult;
+
+            Assert.Equal(2, value.Count);
         }
 
         private static ApiController<T> GetController<T>() where T: class
         {
             var service = Provider.GetService<IService<T>>();
             var listService = Provider.GetService<IListService>();
-            return new ApiController<T>(service, listService);
+            var controller = new ApiController<T>(service, listService);
+            var httpContext = new DefaultHttpContext();
+
+            controller.ControllerContext = new ControllerContext
+            {
+                HttpContext = httpContext
+            };
+
+            return controller;
         }
 
         private static IServiceProvider GetServiceProvider()
---
+++ Api.Net.Tests/_Dtos.cs
@@ -0,0 +1,67 @@
+﻿using Api.Attributes;
+using Api.Dto.Base;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Api.Net.Tests
+{
+    [ApiEndpoint("persons")]
+    public class PersonDto : Dto<PersonDto, Person>
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            return obj is PersonDto dto &&
+                   Id == dto.Id &&
+                   Name == dto.Name &&
+                   Age == dto.Age;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Id, Name, Age);
+        }
+    }
+
+    public class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            return obj is Person dto &&
+                   Id == dto.Id &&
+                   Name == dto.Name &&
+                   Age == dto.Age;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Id, Name, Age);
+        }
+    }
+
+    public class PersonDbContext : DbContext
+    {
+        public PersonDbContext(DbContextOptions<PersonDbContext> options) : base(options) { }
+
+        public DbSet<Person> People { get; set; }
+        
+        public void Clear()
+        {
+            People.RemoveRange(People);
+            SaveChanges();
+        }
+    }
+}
