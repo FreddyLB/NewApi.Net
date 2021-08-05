7f29dff240501064f9a06925abdb2adb52b2bb27
Freddy Luis Paredes B <fparedes@mardom.com>
2021-08-05 08:26:19 -0400

    Added docs to ListResult

--- Api.Net.Core/Models/ListResult.cs
+++ Api.Net.Core/Models/ListResult.cs
@@ -5,5 +5,16 @@ namespace Api.Models
     /// <summary>
     /// Represents the data returned from a request.
     /// </summary>
-    public record ListResult(int Count, IEnumerable Data);
+    public record ListResult(int Count, IEnumerable Data)
+    {
+        /// <summary>
+        /// Number of elements available.
+        /// </summary>
+        public int Count { get; init; } = Count;
+
+        /// <summary>
+        /// An enumerable over the resulting elements.
+        /// </summary>
+        public IEnumerable Data { get; init; } = Data;
+    }
 }
